7f72c254d6d30b9117edf9568bcc6e0bdb592cf4
miteshchodvadiya <miteshchodvadiya1994@gmail.com>
2020-01-20 21:23:04 +0530

    flutter clock completed

---
+++ flutter_digital_clock/lib/digital_clock/digital_clock_screen.dart
@@ -0,0 +1,214 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'dart:ui' as ui;
+import 'package:flutter/services.dart' as ui;
+import 'package:flutter/painting.dart' as ui;
+import 'package:flutter_digital_clock/digital_clock/some_class.dart';
+
+class DigitalClockScreen extends StatefulWidget {
+  @override
+  _DigitalClockScreenState createState() => _DigitalClockScreenState();
+}
+
+class _DigitalClockScreenState extends State<DigitalClockScreen> with TickerProviderStateMixin {
+  AnimationController paddingAnimationController;
+  final _random = new math.Random();
+  var globalKey = new GlobalKey();
+  List<TileObject> tileList = List<TileObject>();
+  ui.Image backGroundImage;
+  ui.Image forGroundImage;
+  Timer timer;
+  int next(int min, int max) => min + _random.nextInt(max - min);
+
+  @override
+  void initState() {
+    paddingAnimationController = new AnimationController(
+      vsync: this,
+      duration: new Duration(milliseconds: 1200),
+    );
+
+    paddingAnimationController.addListener(() {
+      for (var i = 0; i < tileList.length; i++) {
+        var f = tileList[i];
+        f.paddingValue = paddingAnimationController.value;
+      }
+    });
+    setFirstTime();
+    super.initState();
+
+    timer = Timer.periodic(Duration(seconds: 18), (d) {
+      callAnimation();
+    });
+  }
+
+  void setFirstTime() async {
+    await setImageImage();
+    backGroundImage = forGroundImage;
+    await setImageImage();
+    if (mounted) {
+      setState(() {
+        getTileList();
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    paddingAnimationController?.dispose();
+    super.dispose();
+  }
+
+  void getTileList() async {
+    tileList.clear();
+    final colum = next(5, 9);
+    final row = next(3, 7);
+    try {
+      final width = globalKey.currentContext.size.width / colum;
+      final height = globalKey.currentContext.size.height / row;
+      for (var i = 0; i < row; i++) {
+        for (var j = 0; j < colum; j++) {
+          var data = TileObject(
+            animationController: new AnimationController(
+              value: 180,
+              vsync: this,
+              duration: new Duration(milliseconds: 1000),
+            ),
+            size: Size(width, height),
+            offset: Offset(width * j, height * i),
+            only: i == 0 && j == 0 ? true : false,
+          );
+          tileList.add(data);
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AspectRatio(
+              key: globalKey,
+              aspectRatio: 5 / 3,
+              child: Stack(
+                children: <Widget>[
+                  for (var item in tileList)
+                    Positioned(
+                      top: item.offset.dy,
+                      left: item.offset.dx,
+                      width: item.size.width,
+                      height: item.size.height,
+                      child: AnimatedBuilder(
+                        animation: paddingAnimationController,
+                        builder: (BuildContext context, Widget child) {
+                          return AnimatedBuilder(
+                            animation: item.animationController,
+                            builder: (BuildContext context, Widget child) {
+                              return Transform(
+                                transform: Matrix4.identity()
+                                  ..setEntry(3, 2, 0.001)
+                                  ..rotateX(
+                                    degreesToRadians((AlwaysStoppedAnimation(Tween(begin: 0.0, end: 180)
+                                                    .animate(CurvedAnimation(parent: item.animationController, curve: Curves.fastOutSlowIn)))
+                                                .value)
+                                            .value +
+                                        0.0),
+                                  ),
+                                alignment: Alignment.center,
+                                child: RotationTransition(
+                                  turns: AlwaysStoppedAnimation(getRoValue(item: item) / 360),
+                                  child: Transform(
+                                    transform: Matrix4.identity()
+                                      ..setEntry(3, 2, 0.001)
+                                      ..rotateY(degreesToRadians(getRoValue(item: item))),
+                                    alignment: Alignment.center,
+                                    child: CustomPaint(
+                                      painter: CropPainter(
+                                        item.onlyaAnimation ? forGroundImage : backGroundImage,
+                                        globalKey.currentContext,
+                                        tileObject: item,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void callAnimation() {
+    paddingAnimationController.reset();
+    paddingAnimationController.forward().then((f) async {
+      tileList.forEach((f) async {
+        await Future.delayed(Duration(milliseconds: next(240, 2400)));
+        f.animationController.reset();
+        f.animationController.forward().then((d) {
+          f.isDone = true;
+          reset();
+        });
+      });
+    });
+  }
+
+  void reset() async {
+    bool isAllDone = true;
+    tileList.forEach((f) {
+      if (!f.isDone) {
+        isAllDone = false;
+      }
+    });
+    if (isAllDone) {
+      await Future.delayed(const Duration(milliseconds: 240));
+      paddingAnimationController.reverse().then((f) async {
+        await setImageImage();
+        await Future.delayed(const Duration(milliseconds: 240));
+        setState(() {
+          getTileList();
+        });
+      });
+    }
+  }
+
+  Future setImageImage() async {
+    final imageIndex = next(1, 10);
+    var newimage = await ImageLoader.load('assets/clock_image_$imageIndex.jpg');
+    var imageWithText = await SetTextOnImage.getImage(newimage);
+    backGroundImage = forGroundImage;
+    forGroundImage = imageWithText;
+  }
+
+  double getRoValue({TileObject item}) {
+    final newv =
+        (AlwaysStoppedAnimation(Tween(begin: 0.0, end: 180).animate(CurvedAnimation(parent: item.animationController, curve: Curves.fastOutSlowIn)))
+                .value)
+            .value;
+    if (newv != 180) {
+      if (!item.onlyaAnimation && newv > 90) {
+        item.onlyaAnimation = true;
+      }
+    }
+    return newv > 90 ? 180 : 0;
+  }
+
+  double degreesToRadians(double number) {
+    return number * math.pi / 180;
+  }
+}
---
+++ flutter_digital_clock/lib/digital_clock/some_class.dart
@@ -0,0 +1,151 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' as ui;
+import 'package:flutter/painting.dart' as ui;
+import 'package:intl/intl.dart' as date;
+
+class CropPainter extends CustomPainter {
+  final ui.Image backgroundImage;
+  final TileObject tileObject;
+  final BuildContext context;
+
+  CropPainter(this.backgroundImage, this.context, {this.tileObject});
+
+  @override
+  bool shouldRepaint(CropPainter oldDelegate) {
+    return true;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var padding = 2 * tileObject.paddingValue;
+    var w = ((tileObject.size.width - padding) * backgroundImage.width) / context.size.width;
+    var h = ((tileObject.size.height - padding) * backgroundImage.height) / context.size.height;
+    var source = Rect.fromLTWH((w * (tileObject.offset.dx + padding)) / (tileObject.size.width - padding),
+        (h * (tileObject.offset.dy + padding)) / (tileObject.size.height - padding), w, h);
+    canvas.scale(1.01, 1.01);
+    canvas.drawImageRect(
+        backgroundImage, source, Rect.fromLTWH(0, 0, (tileObject.size.width - padding), (tileObject.size.height - padding)), Paint());
+    canvas.clipRect(Rect.fromLTWH(0, 0, size.width, size.height));
+  }
+}
+
+class ImageLoader {
+  static ui.AssetBundle getAssetBundle() => (ui.rootBundle != null) ? ui.rootBundle : new ui.NetworkAssetBundle(new Uri.directory(Uri.base.origin));
+  static Future<ui.Image> load(String url) async {
+    ui.ImageStream stream = new ui.AssetImage(url, bundle: getAssetBundle()).resolve(ui.ImageConfiguration.empty);
+    Completer<ui.Image> completer = new Completer<ui.Image>();
+    ImageStreamListener listener;
+    listener = new ImageStreamListener((ImageInfo frame, bool synchronousCall) {
+      final ui.Image image = frame.image;
+      completer.complete(image);
+      stream.removeListener(listener);
+    });
+    stream.addListener(listener);
+    return completer.future;
+  }
+}
+
+class TileObject {
+  AnimationController animationController;
+  Size size;
+  Offset offset;
+  bool isDone;
+  double paddingValue;
+  bool only;
+  bool onlyaAnimation;
+
+  TileObject({
+    this.animationController,
+    this.size,
+    this.offset,
+    this.isDone = false,
+    this.paddingValue = 0.0,
+    this.only = false,
+    this.onlyaAnimation = false,
+  });
+}
+
+class SetTextOnImage {
+  static Future<ui.Image> getImage(ui.Image forGroundImage) async {
+    final recorder = ui.PictureRecorder();
+    final canvas = Canvas(recorder);
+
+    canvas.drawImage(
+      forGroundImage,
+      Offset(0, 0),
+      Paint(),
+    );
+
+    final timeStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 120,
+      fontWeight: FontWeight.bold,
+    );
+    final timeText = date.DateFormat("h:mm").format(DateTime.now());
+    final timeSpan = TextSpan(
+      text: timeText,
+      style: timeStyle,
+    );
+
+    final timePainter = TextPainter(
+      text: timeSpan,
+      textDirection: TextDirection.ltr,
+    );
+    timePainter.layout(
+      minWidth: 0,
+      maxWidth: 500,
+    );
+    final offset = Offset(50, forGroundImage.height - 200.0);
+    timePainter.paint(canvas, offset);
+
+    final amStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 40,
+      fontWeight: FontWeight.bold,
+    );
+    final amSpan = TextSpan(
+      text: date.DateFormat('a').format(DateTime.now()),
+      style: amStyle,
+    );
+
+    final amPainter = TextPainter(
+      text: amSpan,
+      textDirection: TextDirection.ltr,
+    );
+    amPainter.layout(
+      minWidth: 0,
+      maxWidth: 600,
+    );
+    final amoffset = Offset(timeText.length > 4 ? 364 : 300, forGroundImage.height - 130.0);
+    amPainter.paint(canvas, amoffset);
+
+    final dataStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 40,
+      fontWeight: FontWeight.normal,
+    );
+    final dataSpan = TextSpan(
+      text: date.DateFormat('EEEE, MMM d').format(DateTime.now()) + "  | ⛅ 23°C",
+      style: dataStyle,
+    );
+
+    final dataPainter = TextPainter(
+      text: dataSpan,
+      textDirection: TextDirection.ltr,
+    );
+    dataPainter.layout(
+      minWidth: 0,
+      maxWidth: 500,
+    );
+    final dataoffset = Offset(60, forGroundImage.height - 80.0);
+    dataPainter.paint(canvas, dataoffset);
+
+    final picture = recorder.endRecording();
+    return await picture.toImage(
+      forGroundImage.width.toInt(),
+      forGroundImage.height.toInt(),
+    );
+  }
+}
